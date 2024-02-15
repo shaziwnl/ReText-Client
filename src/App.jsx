9b4a130ac88b3e563d061f35d94cd1ba7f058764
Sharang Goel <goelshaz001@gmail.com>
2024-02-15 00:50:04 -0500

    added copy text functionality

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
   const [concise, setConcise] = useState("")
   const [verbose, setVerbose] = useState("")
   const [highlightedText, setHighlightedText] = useState("")
+  const [copiedText, setCopiedText] = useState("")
 
   var config = { headers: {  
     'Content-Type': 'application/json',
@@ -27,56 +28,63 @@ function App() {
     var selectedText = window.getSelection().toString();
     return selectedText;
   }
-  
 
-  function handleClick() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var activeTab = tabs[0]; // Get the tab we are currently on
+  function sendHighlightedText() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0]; // Get the tab we are currently on
 
-      // Use the chrome.scripting API to inject a content script
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: activeTab.id },
-          function: extractSelectedText
-        },
-        function (result) {
-          // Handle the result returned by the content script
-          var selectedText = result[0].result;
-          setHighlightedText(selectedText)
-          console.log(selectedText);
+    // Use the chrome.scripting API to inject a content script
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: activeTab.id },
+        function: extractSelectedText
+      },
+      function (result) {
+        // Handle the result returned by the content script
+        var selectedText = result[0].result;
+        setHighlightedText(selectedText)
+        console.log(selectedText);
 
-          if (selectedText) {
-            axios.post("https://pennapps.onrender.com/rectify", { sentence: selectedText }, config)
-                .then(res => setRectified(res.data.completion))
+        if (selectedText) {
+          axios.post("https://pennapps.onrender.com/rectify", { sentence: selectedText }, config)
+              .then(res => setRectified(res.data.completion))
 
-            axios.post("https://pennapps.onrender.com/concise", { sentence: selectedText }, config)
-                .then(res => setConcise(res.data.completion))
+          axios.post("https://pennapps.onrender.com/concise", { sentence: selectedText }, config)
+              .then(res => setConcise(res.data.completion))
 
-            axios.post("https://pennapps.onrender.com/verbose", { sentence: selectedText }, config)
-                .then(res => setVerbose(res.data.completion))
-          }
+          axios.post("https://pennapps.onrender.com/verbose", { sentence: selectedText }, config)
+              .then(res => setVerbose(res.data.completion))
         }
-      )
+      }
+    )
     })
-    
   }
 
+
+
   useEffect(() => {
-    handleClick()
+    sendHighlightedText()
   }, [])
 
   useEffect(() => {
     if (useClipboard) {
       navigator.clipboard.readText()
       .then(text => {
-        if (text) {
-          console.log(text)
+        if (text && !highlightedText) {
+          setCopiedText(text)
+          axios.post("https://pennapps.onrender.com/rectify", { sentence: text }, config)
+              .then(res => setRectified(res.data.completion))
+
+          axios.post("https://pennapps.onrender.com/concise", { sentence: text }, config)
+              .then(res => setConcise(res.data.completion))
+
+          axios.post("https://pennapps.onrender.com/verbose", { sentence: text }, config)
+              .then(res => setVerbose(res.data.completion))
         } else {
           console.log("No text copied")
         }
       })
-    }
-    
+    } 
   }, [useClipboard])
 
   useEffect(() => {
@@ -135,11 +143,16 @@ function App() {
 
   return (
     <>
+      <Tooltip title="Uses clipboard if no text is highlighted" placement='right'>
+        <FormControlLabel sx={{marginLeft: "235px"}} 
+          control={<IOSSwitch checked={useClipboard} onChange={() => setUseClipboard(prev => !prev)}/>}
+          label="Use Clipboard"
+        />
+      </Tooltip>
+
       <div className='cards'>
-        <div className = "card">
-          <Tooltip title="Uses clipboard if no text is highlighted" placement='right'>
-            <FormControlLabel control={<IOSSwitch checked={useClipboard} onChange={() => setUseClipboard(prev => !prev)}/>} label="Use Clipboard" />
-          </Tooltip>
+
+        <div className = "card">      
           <h3 className='head'>Rectified</h3>
           <div onMouseEnter={handleMouseEnter1} onMouseLeave={handleMouseLeave1} className="content">
             {isButtonVisible1 && (
@@ -151,7 +164,7 @@ function App() {
                 color='white'
                 /> 
               </div>)}
-            {(!rectified && highlightedText) ?  <Loading/> : rectified}
+            {(!rectified && (highlightedText || copiedText)) ?  <Loading/> : rectified}
           </div>
         </div>
 
@@ -168,7 +181,7 @@ function App() {
                 color='white'
                 />
               </div>)}
-            {(!concise && highlightedText) ?  <Loading/> : concise}
+            {(!concise && (highlightedText || copiedText)) ?  <Loading/> : concise}
           </div>
 
           <h4 className="sub-head">Clearer, more verbose</h4>
@@ -182,7 +195,7 @@ function App() {
                 color='white'
                 />
               </div>)}
-            {(!verbose && highlightedText) ?  <Loading/> : verbose}
+            {(!verbose && (highlightedText || copiedText)) ?  <Loading/> : verbose}
           </div>
         </div>
 
